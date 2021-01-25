6eaebe71546c5f24f9ded017502719b8f0a6138b
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-01-25 15:26:54 +0900

    build: add renovate

---
+++ .github/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	branchPrefix: "renovate/",
+	dryRun: true,
+	gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+	onboarding: true,
+	onboardingBranch: "renovate/configure",
+	platform: "github",
+	includeForks: true,
+  repositories: ["kuro-kuroite/test-renovate-self-hosted"],
+};
