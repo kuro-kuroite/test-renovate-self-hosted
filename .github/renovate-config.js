dbf06634488040dfa3c8512f65d3a08e54c628f5
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-01-25 17:23:30 +0900

    fix: fix renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,8 +1,5 @@
 module.exports = {
-	branchPrefix: "renovate/",
 	gitAuthor: "Renovate Bot <bot@renovateapp.com>",
-	onboarding: true,
-	onboardingBranch: "renovate/configure",
 	platform: "github",
 	includeForks: true,
   repositories: ["kuro-kuroite/test-renovate-self-hosted"],
