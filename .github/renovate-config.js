16088c896866233e3ca1921a74a48c91d50363ff
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-01-26 16:16:47 +0900

    fix: fix renovate.yml

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -2,7 +2,6 @@ module.exports = {
 	gitAuthor: "Renovate Bot <renovate@whitesourcesoftware.com>",
 	platform: "github",
 	onboarding: false,
-	branchPrefix: "renovate-",
 	includeForks: true,
   repositories: ["kuro-kuroite/test-renovate-self-hosted"],
 };
